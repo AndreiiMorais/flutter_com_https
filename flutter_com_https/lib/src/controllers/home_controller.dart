2c7f18f9c3a72c378f57ae598f68b0d4c3cffd82
Andrei Silveira Morais <andrei.morais@outlook.com>
2021-12-18 00:11:56 -0300

    finalizado
    reatividade, view e mais

--- flutter_com_https/lib/src/controllers/home_controller.dart
+++ flutter_com_https/lib/src/controllers/home_controller.dart
@@ -1,11 +1,22 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter_com_https/src/models/todo_model.dart';
 import 'package:flutter_com_https/src/repositories/todo_repository.dart';
 
 class HomeController {
   List<ToDoModel> todos = [];
   final repository = ToDoRepository();
+  final state = ValueNotifier<Homestates>(Homestates.start);
 
   Future start() async {
-    todos = await repository.fetchToDos();
+    state.value = Homestates.loading;
+    try {
+      todos = await repository.fetchToDos();
+      state.value = Homestates.sucess;
+    } on Exception catch (e) {
+      state.value = Homestates.error;
+      // TODO
+    }
   }
 }
+
+enum Homestates { start, loading, sucess, error }
--- flutter_com_https/lib/src/home_page.dart
+++ flutter_com_https/lib/src/home_page.dart
@@ -1,22 +1,92 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_com_https/src/controllers/home_controller.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final controller = HomeController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('List ToDo\'s'),
-      ),
-      body: ListView.builder(
-        //o listview.builder cria um array com a quantidade de itemcount que tiver ja com scroll para a tela.
-        itemCount: 20,
-        itemBuilder: (context, index) => ListTile(
-          title: Text('item $index'),
+        appBar: AppBar(
+          title: Text('List ToDo\'s'),
+          actions: [
+            IconButton(
+              icon: Icon(
+                Icons.refresh,
+              ),
+              onPressed: () {
+                controller.start();
+              },
+            ),
+          ],
         ),
+        body: AnimatedBuilder(
+          animation: controller.state,
+          builder: (context, child) {
+            return stateManager(controller.state.value);
+          },
+        ));
+  }
+
+  initState() {
+    super.initState();
+    controller.start();
+  }
+
+  _sucess() {
+    return ListView.builder(
+      //o listview.builder cria um array com a quantidade de itemcount que tiver ja com scroll para a tela.
+      itemCount: controller.todos.length,
+      itemBuilder: (context, index) {
+        var todo = controller.todos[index];
+        return ListTile(
+          title: Text(todo.title),
+        );
+      },
+    );
+  }
+
+  _error() {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {
+          controller.start();
+        },
+        child: Text('Tentar Novamente'),
       ),
     );
+  }
+
+  _start() {
+    Container();
+  }
+
+  _loading() {
+    return Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+
+  stateManager(Homestates state) {
+    switch (state) {
+      case Homestates.start:
+        return _start();
+      case Homestates.loading:
+        return _loading();
+      case Homestates.error:
+        return _error();
+      case Homestates.sucess:
+        return _sucess();
+      default:
+        _start();
+    }
   }
 }
